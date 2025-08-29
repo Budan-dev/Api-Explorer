51a621708105df50a3e8932a6977e1b06262acb2
Suleman Bababudan <bababudansuleman@gmail.com>
2025-08-29 20:48:08 +0100

    Changes to the Router

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Api-Explorer">
       <Header />
       <Routes>
         <Route path="/" element={<Main />} />
